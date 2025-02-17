894d3c60751c02830b843cbc80aa87eb936ad02c
Sarah Converse <sconver@uw.edu>
2025-02-17 01:09:04 -0800

    new indvar approach

---
+++ scripts/occupancy_singlespp_model_indvar_simplerdetection.R
@@ -0,0 +1,193 @@
+library(here)
+library(nimble)
+library(MCMCvis)
+library(coda)
+
+
+start.time <- Sys.time() 
+
+# MCMC settings
+ni <- 40000
+nt <- 1
+nb <- 5000
+nc <- 3
+
+#import data 
+data1 <- read.csv("data/bird.binary.noCorrRes.csv") 
+
+#set up sites, site type, years, species 
+#sites - put in alphabetical order 
+sites <- sort(unique(data1$Site))
+#site type - note that CD sites are 1 and PCD sites are 2
+site.type <- sites
+for(s in 1:length(sites)){
+  site.type[s] <- data1$Site.Type[max(which(data1$Site == sites[s]))]
+}
+site.type <- as.numeric(as.factor(site.type)) 
+#years - put in order 
+years <- sort(unique(data1$Year))
+species <- c(9:61)
+
+#set up observation data dim = sites, years, species, reps 
+array2 <- array(NA,dim= c(7,12,53,32))
+for(j in 1:length(sites)){
+  for(y in 1:length(years)){
+    for(k in 1:length(species)){
+      nums <- intersect(which(data1$Site == sites[j]),which(data1$Year == years[y]))
+      if(length(nums)>0){
+        array2[j,y,k,1:length(nums)] <- data1[nums,species[k]]
+      }
+    }
+  }
+}
+
+#effort array - make effort 0 anytime the data are NA  
+array.eff <- array(1,dim=c(7,12,53,32))
+array.eff[is.na(array2==TRUE)] <- 0 
+#now make the data that are NAs 0s 
+array2[is.na(array2==TRUE)] <- 0
+
+#take out species with a small sample size 
+array.eff <- array.eff[,,-c(10,12,19,21,29,36,39,44,45,50),]
+array2 <- array2[,,-c(10,12,19,21,29,36,39,44,45,50),]
+#species names 
+spp.names <- colnames(data1)[9:61][-c(10,12,19,21,29,36,39,44,45,50)]
+
+species <- dim(array2)[3]
+#store gelman diagnostics 
+R.hat <- rep(NA,species)
+
+#loop through species 
+for(i in 1:species){
+
+#pull out data for single species analysis
+spp.i <- array2[,,i,]
+eff.i <- array.eff[,,i,]
+
+##NIMBLE code 
+occ1 <- nimbleCode( { 
+
+#Likelihood
+for(s in 1:n.sites) {
+  for(y in 1:n.years){
+    # State Process  
+      z[s,y] ~ dbern(psi[s,y])
+      # Observation Process  
+      for(n in 1:n.reps) {
+        spp.i[s,y,n] ~ dbern(z[s,y] * p[s,y] * eff.i[s,y,n])  #state * p * effort (0 or 1)
+      }#n replicate
+  }#y year
+}#j site 
+
+
+#model on detection
+#model on occupancy 
+for(s in 1:n.sites){
+  for(y in 1:n.years){
+
+    logit(p[s,y]) <- int.p + p.site.type[site.type[s]] + p.ran.year[y]
+      
+    logit(psi[s,y]) <- int.psi + w.sitetype*beta.site.type[site.type[s]] + w.year*beta.year*year.norm[y] + w.year2*beta.year2*year2.norm[y] 
+  }
+}
+
+#Random effects 
+for(y in 1:n.years){
+  p.ran.year[y] ~ dnorm(0,sd = sd.year)
+}
+  
+#Priors 
+int.p ~ dnorm(0,sd=1)
+p.site.type[1] ~ dnorm(0,sd=1)
+p.site.type[2] <- -1*p.site.type[1]
+sd.year ~ dgamma(1,1)
+
+w.sitetype ~ dbern(2/3)
+w.year ~ dbern(1/2)
+w.year2 <- w.year*w.yearsq
+w.yearsq ~ dbern(1/2)
+
+int.psi ~ dnorm(0,sd=1)
+beta.site.type[1] <- 0
+beta.site.type[2] ~ dnorm(0,sd=1)
+beta.year ~ dnorm(0,sd=1)
+beta.year2 ~ dnorm(0,sd=1)
+
+})
+
+
+######################################################################
+#                                                                    #  
+#                      Data and Constants                            #
+#                                                                    #    
+######################################################################
+
+# Bundle data
+data <- list(spp.i=spp.i)
+
+#get constants 
+n.sites <- dim(spp.i)[1]
+n.years <- dim(spp.i)[2]
+n.reps <- dim(spp.i)[3]
+year.norm <- (c(1:12) - mean(c(1:12)))/sd(c(1:12))
+year2.norm <- pow(year.norm,2)
+
+constants <- list(eff.i=eff.i,site.type=site.type,year.norm=year.norm,year2.norm=year2.norm,n.sites=n.sites,n.years=n.years,n.reps=n.reps)
+
+######################################################################
+#                                                                    #  
+#                             Inits                                  #
+#                                                                    #    
+######################################################################
+
+z.init <- matrix(0,nrow=n.sites,ncol=n.years)
+for(s in 1:n.sites){
+  for(y in 1:n.years){
+    if(any(spp.i[s,y,]==1)){
+      z.init[s,y] <- 1 
+    }  
+  }
+}
+inits <- list(z=z.init)
+
+######################################################################
+#                                                                    #  
+#                         Run the model                              #
+#                                                                    #    
+######################################################################
+
+# Parameters monitored
+params <- c("int.p","int.psi","beta.site.type","beta.year","beta.year2","w.sitetype","w.year","w.year2") 
+
+Rmodel1 <- nimbleModel(code = occ1, constants = constants, data = data,
+                       check = FALSE, calculate = FALSE, inits = inits)
+conf1 <- configureMCMC(Rmodel1, monitors = params, thin = nt, useConjugacy = FALSE, enableWAIC=TRUE)
+Rmcmc1 <- buildMCMC(conf1)
+Cmodel1 <- compileNimble(Rmodel1, showCompilerOutput = FALSE)
+Cmcmc1 <- compileNimble(Rmcmc1, project = Rmodel1)
+
+## Run MCMC ####
+out <- runMCMC(Cmcmc1, niter = ni, nburnin = nb , nchains = nc, inits = inits,
+               setSeed = FALSE, progressBar = TRUE, samplesAsCodaMCMC = TRUE, WAIC=TRUE)
+
+out.all <- rbind(out$samples$chain1,out$samples$chain2,out$samples$chain3)
+
+R.hat[i] <- gelman.diag(out$samples[,c(2,3,4,5,6,7,8)],multivariate=TRUE)$mpsrf
+
+write.csv(out.all,paste("results/occ.",spp.names[i], ".csv",sep=""))
+
+print(i)
+
+}
+
+
+R.hat 
+
+
+
+
+
+
+
+
+
--- scripts/process_occupancy_results.R
+++ scripts/process_occupancy_results.R
@@ -1,13 +1,13 @@
 #read in results 
 
-model1 <- as.matrix(read.csv("results/occ_model1_results.csv"))
-model2 <- as.matrix(read.csv("results/occ_model2_results.csv"))
-model3 <- as.matrix(read.csv("results/occ_model3_results.csv"))
-model4 <- as.matrix(read.csv("results/occ_model4_results.csv"))
-model5 <- as.matrix(read.csv("results/occ_model5_results.csv"))
-model6 <- as.matrix(read.csv("results/occ_model6_results.csv"))
-model7 <- as.matrix(read.csv("results/occ_model7_results.csv"))
-model8 <- as.matrix(read.csv("results/occ_model8_results.csv"))
+model1 <- as.matrix(read.csv("results/occ_model1_results.csv"))[,-c(1)]
+model2 <- as.matrix(read.csv("results/occ_model2_results.csv"))[,-c(1)]
+model3 <- as.matrix(read.csv("results/occ_model3_results.csv"))[,-c(1)]
+model4 <- as.matrix(read.csv("results/occ_model4_results.csv"))[,-c(1)]
+model5 <- as.matrix(read.csv("results/occ_model5_results.csv"))[,-c(1)]
+model6 <- as.matrix(read.csv("results/occ_model6_results.csv"))[,-c(1)]
+model7 <- as.matrix(read.csv("results/occ_model7_results.csv"))[,-c(1)]
+model8 <- as.matrix(read.csv("results/occ_model8_results.csv"))[,-c(1)]
 
 #read in data - where is sample size perhaps too small? 
 data1 <- read.csv("data/bird.binary.noCorrRes.csv")
@@ -40,6 +40,10 @@ max(model6[,"R.hat"])
 max(model7[,"R.hat"])
 max(model8[,"R.hat"])
 
+avoider.list <- avoider.list[which(is.element(avoider.list,data.poor)==FALSE)]
+adapter.list <- adapter.list[which(is.element(adapter.list,data.poor)==FALSE)]
+exploiter.list <- exploiter.list[which(is.element(exploiter.list,data.poor)==FALSE)]
+
 #best model 
                                   
 mod.selection <- cbind(model1[,"WAIC"],model2[,"WAIC"],model3[,"WAIC"],model4[,"WAIC"],model5[,"WAIC"],model6[,"WAIC"],model7[,"WAIC"],model8[,"WAIC"])
@@ -50,30 +54,56 @@ for(i in 1:nrow(mod.selection)){
   delta.mod.selection[i,] <- mod.selection[i,] - min(mod.selection[i,])
 }
 
-  
-best.model <- apply(mod.selection,1,which.max)
+avoider.selection <- delta.mod.selection[avoider.list,]
+adapter.selection <- delta.mod.selection[adapter.list,]
+exploiter.selection <- delta.mod.selection[exploiter.list,]
 
-best.model.avoider <- best.model[avoider.list]
-best.model.adapter <- best.model[adapter.list]
-best.model.exploiter <- best.model[exploiter.list]
-
-preds <- array(NA, dim = c(53,127,8))
-preds[,1:ncol(model1),1] <- model1 
-preds[,1:ncol(model2),2] <- model2 
-preds[,1:ncol(model3),3] <- model3 
-preds[,1:ncol(model4),4] <- model4 
-preds[,1:ncol(model5),5] <- model5 
-preds[,1:ncol(model6),6] <- model6 
-preds[,1:ncol(model7),7] <- model7 
-preds[,1:ncol(model8),8] <- model8 
-
-preds.best.model <- array(NA,dim=c(43,24,4))
-for(i in 1:43){
-  preds.best.model[i,c(1:24),1] <- preds[i,c(1:24),best.model[i]]
-  preds.best.model[i,c(1:24),2] <- preds[i,c(25:48),best.model[i]]
-  preds.best.model[i,c(1:24),3] <- preds[i,c(49:72),best.model[i]]
-  preds.best.model[i,c(1:24),4] <- preds[i,c(73:96),best.model[i]]
+#write a function to get the inference model for each guild 
+#this is the best model including site type 
+inf.model.fxn <- function(x){
+inf.model.out <- rep(NA,nrow(x))
+for(i in 1:nrow(x)){
+  ord <- order(x[i,])
+  for(j in 1:length(ord)){
+    if(is.element(ord[j],c(2,5,6,7,8))){
+      inf.model.out[i] <- ord[j];      
+      break      
+    }
+  } 
 }
+return(inf.model.out)
+}
+inf.model.avoider <- inf.model.fxn(avoider.selection)
+inf.model.adapter <- inf.model.fxn(adapter.selection)
+inf.model.exploiter <- inf.model.fxn(exploiter.selection)
+
+#get all predictions, including mean, SD, LCI, and UCI for species and models 
+preds <- array(NA, dim = c(53,96,8))
+preds[,,1] <- model1[,1:96]
+preds[,,2] <- model2[,1:96]
+preds[,,3] <- model3[,1:96]
+preds[,,4] <- model4[,1:96]
+preds[,,5] <- model5[,1:96]
+preds[,,6] <- model6[,1:96]
+preds[,,7] <- model7[,1:96]
+preds[,,8] <- model8[,1:96]
+
+#best model - this is the model that is best for each species and contains a site effect 
+
+#get the predictions from the best model 
+preds.best.model <- function(preds,best.model,list){
+  preds.best <- array(NA,dim = c(length(list),24,4))
+  for(i in 1:length(list)){
+    preds.best[i,c(1:24),1] <- preds[list[i],c(1:24),best.model[i]]
+    preds.best[i,c(1:24),2] <- preds[list[i],c(25:48),best.model[i]]
+    preds.best[i,c(1:24),3] <- preds[list[i],c(49:72),best.model[i]]
+    preds.best[i,c(1:24),4] <- preds[list[i],c(73:96),best.model[i]]
+  }
+return(preds.best)  
+}
+preds.avoider <- preds.best.model(preds,inf.model.avoider,avoider.list)
+preds.adapter <- preds.best.model(preds,inf.model.adapter,adapter.list)
+preds.exploiter <- preds.best.model(preds,inf.model.exploiter,exploiter.list)
 
 #params by model 
 #Model1: "int.p","int.psi"
@@ -85,9 +115,55 @@ for(i in 1:43){
 #Model7: "int.p","int.psi","beta.site.type[2]","beta.year","beta.site.type.year[2]"
 #Model8: "int.p","int.psi","beta.site.type[2]","beta.year","beta.year2","beta.site.type.year[2]","beta.site.type.year2[2]"
 
+coef <- array(NA, dim = c(53,28,8))
+coef[,1:length(97:(ncol(model1)-2)),1] <- model1[,(97:(ncol(model1)-2))] 
+coef[,1:length(97:(ncol(model2)-2)),2] <- model2[,(97:(ncol(model2)-2))] 
+coef[,1:length(97:(ncol(model3)-2)),3] <- model3[,(97:(ncol(model3)-2))] 
+coef[,1:length(97:(ncol(model4)-2)),4] <- model4[,(97:(ncol(model4)-2))] 
+coef[,1:length(97:(ncol(model5)-2)),5] <- model5[,(97:(ncol(model5)-2))] 
+coef[,1:length(97:(ncol(model6)-2)),6] <- model6[,(97:(ncol(model6)-2))] 
+coef[,1:length(97:(ncol(model7)-2)),7] <- model7[,(97:(ncol(model7)-2))] 
+coef[,1:length(97:(ncol(model8)-2)),8] <- model8[,(97:(ncol(model8)-2))] 
 
-avoider.list <- avoider.list[which(is.element(avoider.list,data.poor)==FALSE)]
-adaptor.list <- adaptor.list[which(is.element(adaptor.list,data.poor)==FALSE)]
-exploiter.list <- exploiter.list[which(is.element(exploiter.list,data.poor)==FALSE)]
+coef.best.model <- function(coef,best.model,list){
+  coef.best <- array(NA,dim = c(length(list),7,4))
+  for(i in 1:length(list)){
+    if(best.model[i] == 2){
+      coef.best[i,c(1:3),1] <- coef[list[i],c(1:3),best.model[i]]
+      coef.best[i,c(1:3),2] <- coef[list[i],c(4:6),best.model[i]]
+      coef.best[i,c(1:3),3] <- coef[list[i],c(7:9),best.model[i]]
+      coef.best[i,c(1:3),4] <- coef[list[i],c(10:12),best.model[i]]
+    }else if(best.model[i] == 5){
+      coef.best[i,c(1:4),1] <- coef[list[i],c(1:4),best.model[i]]
+      coef.best[i,c(1:4),2] <- coef[list[i],c(5:8),best.model[i]]
+      coef.best[i,c(1:4),3] <- coef[list[i],c(9:12),best.model[i]]
+      coef.best[i,c(1:4),4] <- coef[list[i],c(13:16),best.model[i]]
+    }else if(best.model[i] == 6){
+      coef.best[i,c(1:5),1] <- coef[list[i],c(1:5),best.model[i]]
+      coef.best[i,c(1:5),2] <- coef[list[i],c(6:10),best.model[i]]
+      coef.best[i,c(1:5),3] <- coef[list[i],c(11:15),best.model[i]]
+      coef.best[i,c(1:5),4] <- coef[list[i],c(16:20),best.model[i]]
+    }else if(best.model[i] == 7){
+      coef.best[i,c(1:5),1] <- coef[list[i],c(1:5),best.model[i]]
+      coef.best[i,c(1:5),2] <- coef[list[i],c(6:10),best.model[i]]
+      coef.best[i,c(1:5),3] <- coef[list[i],c(11:15),best.model[i]]
+      coef.best[i,c(1:5),4] <- coef[list[i],c(16:20),best.model[i]]
+    }else if(best.model[i] == 8){
+      coef.best[i,c(1:7),1] <- coef[list[i],c(1:7),best.model[i]]
+      coef.best[i,c(1:7),2] <- coef[list[i],c(8:14),best.model[i]]
+      coef.best[i,c(1:7),3] <- coef[list[i],c(15:21),best.model[i]]
+      coef.best[i,c(1:7),4] <- coef[list[i],c(22:28),best.model[i]]
+    }   
+  }    
+  return(coef.best)  
+}
+coef.avoider <- coef.best.model(coef,inf.model.avoider,avoider.list)
+coef.adapter <- coef.best.model(coef,inf.model.adapter,adapter.list)
+coef.exploiter <- coef.best.model(coef,inf.model.exploiter,exploiter.list)
 
-
+coef.avoider.compact <- matrix(NA,nrow=dim(coef.avoider)[1],ncol=dim(coef.avoider)[2])
+for(i in 1:dim(coef.avoider)[1]){
+  for(j in 1:dim(coef.avoider)[2]){
+    coef.avoider.compact[i,j] <- paste(round(coef.avoider[i,j,1],dig=2),round(coef.avoider[i,j,3],dig=2),round(coef.avoider[i,j,4],dig=2),sep=",")
+  }
+}
